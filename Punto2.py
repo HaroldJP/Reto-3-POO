1fc32c9bfddac1fdeec6d3bc7c8dd7f220a06c51
HaroldJP <142249299+HaroldJP@users.noreply.github.com>
2024-03-15 14:57:29 -0500

    Update Punto2.py

--- Punto2.py
+++ Punto2.py
@@ -4,16 +4,24 @@ class MenuItem:
         self.price = price
 
 class Beverage(MenuItem):
-    pass
+    def __init__(self, name, price, kind):
+        super().__init__(name, price)
+        self.kind = kind
 
 class FastFood(MenuItem):
-    pass
+    def __init__(self, name, price, kind):
+        super().__init__(name, price)
+        self.kind = kind
 
 class Lunch(MenuItem):
-    pass
+    def __init__(self, name, price, kind):
+        super().__init__(name, price)
+        self.kind = kind
 
 class Protein(MenuItem):
-    pass
+    def __init__(self, name, price, kind):
+        super().__init__(name, price)
+        self.kind = kind
 
 class Order:
     def __init__(self, items):
@@ -25,24 +33,24 @@ class Order:
 
 if __name__=="__main__":
 
-    coca_cola = Beverage(name="Coca-cola", price=3500)
-    orange_juice = Beverage(name="Orange juice", price=3000)
-    lemonade = Beverage(name="Lemonade", price=2500)
-    water = Beverage(name="Water", price=2000)
-    coffee = Beverage(name="Coffee", price=2500)
-    chocolate = Beverage(name="Chocolate", price=2500)
+    coca_cola = Beverage(name="Coca-cola", price=3500, kind = "Cold beverage")
+    orange_juice = Beverage(name="Orange juice", price=3000, kind = "Cold beverage")
+    lemonade = Beverage(name="Lemonade", price=2500, kind = "Cold beverage")
+    water = Beverage(name="Water", price=2000, kind = "Cold beverage")
+    coffee = Beverage(name="Coffee", price=2500, kind = "Hot beverage")
+    chocolate = Beverage(name="Chocolate", price=2500, kind = "Hot beverage")
 
-    hot_dog = FastFood(name="Hot dog", price=10000)
-    hamburguer = FastFood(name="Hamburguer", price=12000)
-    pizza = FastFood(name="Pizza", price=8000)
+    hot_dog = FastFood(name="Hot dog", price=10000, kind = "Big sausace")
+    hamburguer = FastFood(name="Hamburguer", price=12000, kind = "Double beef")
+    pizza = FastFood(name="Pizza", price=8000, kind = "Extra cheese")
 
-    lentils = Lunch(name="Lentils with rice and salad", price=7000)
-    beans = Lunch(name="Beans with rice and salad", price=7000)
-    pasta = Lunch(name="Pasta with bread and salad", price=6500)
+    lentils = Lunch(name="Lentils with rice and salad", price=7000, kind = "Ordinary")
+    beans = Lunch(name="Beans with rice and salad", price=7000, kind = "Ordinary")
+    pasta = Lunch(name="Pasta with bread and salad", price=6500, kind = "Executive")
 
-    beef = Protein(name="Beef", price=6000)
-    chicken = Protein(name="Chicken", price=5500)
-    fish = Protein(name="Mojarra", price=7800)
+    beef = Protein(name="Beef", price=6000, kind = "Cow")
+    chicken = Protein(name="Chicken", price=5500, kind = "Chicken meal")
+    fish = Protein(name="Mojarra", price=7800, kind = "Fish meal")
 
     # Enter your order
     your_order = [water, lentils, chicken, pizza]
